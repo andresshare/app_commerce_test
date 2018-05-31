3c047944f50fb7edbc6f7ba720fb95f005a14db0
andresshare <andresshare@gmail.com>
2018-05-31 18:57:31 -0500

    modified templates

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,4 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request,"home_page.html", {})
+    context = {
+        "title": "Test message",
+        "content": "Welcome to home_page"
+    }
+    return render(request,"home_page.html",context)
+
+
+def about_page(request):
+    context = {
+        "title": "about_page",
+        "content": "Welcome to about_page"
+    }
+    return render(request, "about_page.html", context)
+
+
+def contact_page(request):
+    context = {
+        "title": "contact_page",
+        "content": "Welcome to contact_page"
+    }
+    return render(request, "contact_page.html",context)
