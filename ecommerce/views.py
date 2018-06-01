90d659f0ce9226f381400c1d97381388f6c5c311
andresshare <andresshare@gmail.com>
2018-05-31 20:25:26 -0500

    modified forms-look-widgett

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def home_page(request):
     context = {
         "title": "Test message",
@@ -18,14 +20,22 @@ def about_page(request):
 
 
 def contact_page(request):
+    #instance form
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "contact_page",
-        "content": "Welcome to contact_page"
+        "content": "Welcome to contact_page",
+        "form": contact_form
     }
+
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+
     #test_to_ send_data
-    if request.method == 'POST':
-        print(request.POST)
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+    #if request.method == 'POST':
+        #print(request.POST)
+        #print(request.POST.get('fullname'))
+        #print(request.POST.get('email'))
+        #print(request.POST.get('content'))
     return render(request, "contact/view.html",context)
+
