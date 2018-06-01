8a0722952a789bbc53bcac807157c7f362d3a51a
andresshare <andresshare@gmail.com>
2018-05-31 21:55:36 -0500

    modified auth/allows login

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,13 +1,16 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm
 
 def home_page(request):
     context = {
         "title": "Test message",
-        "content": "Welcome to home_page"
+        "content": "Welcome to home_page",
+        "premium_content":"you hace access rigth PREMIUM"
     }
+
     return render(request,"home_page.html",context)
 
 
@@ -39,3 +42,36 @@ def contact_page(request):
         #print(request.POST.get('content'))
     return render(request, "contact/view.html",context)
 
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print("User logged in")
+    #print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        #print(request.user.is_authenticated())
+        if user is not None:
+            #print(request.user.is_authenticated())
+            login(request, user)
+        # Redirect to a success page.
+            return redirect("/login")
+        else:
+        # Return an 'invalid login' error message.
+            print("Error")
+
+    return render(request, "auth/login.html", context)
+
+
+def register_page(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "auth/register.html", {})
