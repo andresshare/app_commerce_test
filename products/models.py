95e37eaa1345472212bd7af80f5a496ca6f06924
andresshare <andresshare@gmail.com>
2018-06-03 21:27:12 -0500

    activate feature

--- products/models.py
+++ products/models.py
@@ -18,7 +18,24 @@ def upload_image_path(instance, filename):
                                                     new_filename=new_filename,
                                                     final_filename=final_filename
                                                  )
+
+class ProductQuerySet(models.query.QuerySet):
+    def activate(self):
+        return self.filter(featured=True, activate=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
 class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().featured()
+
+    def featured(self): #product.objects.featured()
+        return self.get_queryset().featured()
+
     def get_by_id(self, id):
         qs = self.get_queryset().filter(id=id)
         if qs.count() == 1:
@@ -32,6 +49,9 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+    featured = models.BooleanField(default=False)
+    activate = models.BooleanField(default=True)
+
     objects = ProductManager()
     def __str__(self):
         return self.title
--- products/views.py
+++ products/views.py
@@ -7,6 +7,22 @@ from django.shortcuts import render,get_object_or_404
 
 from .models import Product
 
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwars):
+        request = self.request
+        return Product.objects.all().featured()
+
+class ProductFeaturedDetailView(ListView):
+    template_name = "products/featured-detail.html"
+
+    def get_queryset(self, *args, **kwars):
+        request = self.request
+        return Product.objects.all().featured()
+
+
+
 class ProductListView(ListView):
     #queryset = Product.objects.all()
     template_name = "products/list.html"
