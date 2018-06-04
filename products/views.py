44099684ec5105cc5d0e6eb01801ad715873788a
andresshare <andresshare@gmail.com>
2018-06-03 20:14:06 -0500

    modified models/ detail views

--- products/views.py
+++ products/views.py
@@ -8,13 +8,16 @@ from django.shortcuts import render,get_object_or_404
 from .models import Product
 
 class ProductListView(ListView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/list.html"
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data( *args, **kwargs)
     #     print(context)
     #     return context
+    def get_queryset(self, *args, **kwars):
+        request = self.request
+        return Product.objects.all()
 
 def product_list_view(request):
     queryset = Product.objects.all()
@@ -25,7 +28,7 @@ def product_list_view(request):
 
 #DetailView
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
@@ -33,18 +36,34 @@ class ProductDetailView(DetailView):
         print(context)
         return context
 
+    # def get_object(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #      instance = Product.objects.get_by_id(pk)
+    #     if instance is None:
+    #          raise Http404("Product_doesnt exists")
+    #     return instance
+
+    def get_queryset(self, *args, **kwars):
+        request = self.request
+        pk =self.kwars.get('pk')
+        return Product.objects.filter(pk=pk)
 
 def product_detail_view(request, pk=None, *args, **kwargs):
-    #instance = Product.objects.get(pk=pk) #id
-        qs = Product.objects.filter(id=pk)
+        #instance = Product.objects.get(pk=pk) #id
+        # instance = Product.objects.get_by_id(pk)
+        # qs = Product.objects.filter(id=pk)
+
         #print(qs)
-        if qs.exists() and qs.count() == 1:
-           instance = qs.first()
-        else:
-            raise Http404("Product_doesnt exists")
+        # if qs.exists() and qs.count() == 1:
+        #    instance = qs.first()
+        # else:
+        #     raise Http404("Product_doesnt exists")
+        # instance = get_object_or_404(Product,pk=pk)
 
-
-        instance = get_object_or_404(Product,pk=pk)
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+             raise Http404("Product_doesnt exists")
         context = {
             'object': instance
         }
